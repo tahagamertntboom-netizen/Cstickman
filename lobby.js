cf42f788a045cebbef4b06ea478a8a6a167a3753
tahagamertntboom-netizen <tahagamertntboom@gmail.com>
2026-08-15 04:58:00 -0700

    Update lobby.js

--- lobby.js
+++ lobby.js
@@ -1,107 +1,12 @@
-let socket = new WebSocket(
-    "ws://localhost:8080"
-);
+function enterLobby() {
+  let name = document.getElementById("playerName").value;
 
+  if (name.trim() === "") {
+    alert("اسم وارد کن");
+    return;
+  }
 
-socket.onopen = function(){
+  localStorage.setItem("playerName", name);
 
-    console.log("وصل شد");
-
-};
-
-
-
-function createRoom(){
-
-
-    let name =
-    document.getElementById("name").value;
-
-
-    console.log(
-        "ساخت لابی:",
-        name
-    );
-
-
-    socket.send(JSON.stringify({
-
-        type:"create",
-        name:name
-
-    }));
-
+  window.location.href = "game.html";
 }
-
-
-
-function joinRoom(){
-
-
-    let name =
-    document.getElementById("name").value;
-
-
-    let code =
-    document.getElementById("code").value;
-
-
-
-    socket.send(JSON.stringify({
-
-        type:"join",
-        name:name,
-        code:code
-
-    }));
-
-}
-
-
-
-
-socket.onmessage=function(event){
-
-
-    console.log(
-        "سرور:",
-        event.data
-    );
-
-
-    let data =
-    JSON.parse(event.data);
-
-
-
-    if(data.type==="room"){
-
-
-        document.getElementById("room").innerHTML =
-
-        "کد لابی: " + data.code;
-
-
-    }
-
-
-
-    if(data.type==="joined"){
-
-
-        window.location.href="game.html";
-
-
-    }
-
-
-
-    if(data.type==="error"){
-
-
-        alert(data.message);
-
-    }
-
-
-};
